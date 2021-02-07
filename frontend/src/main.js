3f0b54d808f9a36164380236f23b21f4215f6462
Nahuel Ambrosini <nambrosini@onapsis.com>
2021-02-07 16:25:06 -0300

    ✨ Add output of vue add i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/styles/index.css';
+import i18n from './i18n'
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
