ab4fb0595afe034541a22b6cea2edd4b21ba2896
Nahuel Ambrosini <nambrosini@onapsis.com>
2021-02-10 21:36:54 -0300

    🗑️ Remove unused code

--- frontend/src/components/LangSwitcher.vue
+++ frontend/src/components/LangSwitcher.vue
@@ -7,7 +7,6 @@
       @click="toggleVisibility"
       @keydown.esc.exact="hideDropdown"
     >
-        <!-- <span class="ml-2">{{ $i18n.locale }}</span> -->
         <svg class="h-5 w-5 sm:h-6 sm:w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
@@ -62,26 +61,6 @@ export default {
     hideDropdown() {
       this.isVisible = false
       this.focusedIndex = 0
-    },
-    startArrowKeys() {
-      if (this.isVisible) {
-        this.$refs.dropdown.children[0].children[0].focus()
-      }
-    },
-    focusPrevious(isArrowKey) {
-      this.focusedIndex = this.focusedIndex - 1
-      if (isArrowKey) {
-        this.focusItem()
-      }
-    },
-    focusNext(isArrowKey) {
-      this.focusedIndex = this.focusedIndex + 1
-      if (isArrowKey) {
-        this.focusItem()
-      }
-    },
-    focusItem() {
-      this.$refs.dropdown.children[this.focusedIndex].children[0].focus()
     },
     setLocale(locale) {
       this.$i18n.locale = locale
