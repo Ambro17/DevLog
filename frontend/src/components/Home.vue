5fb6c3aaa3a9e30d2cbd2635fa6ec30c154c67af
Nahuel Ambrosini <nambrosini@onapsis.com>
2021-02-10 21:36:18 -0300

    🔧 Revert edit

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -88,7 +88,7 @@
 
           <a class="mt-8 max-w-2xl w-full block bg-white shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
             <div class="flex items-center justify-between px-4 py-2">
-              <h3 class="text-lg text-center font-medium text-gray-700">Strawberry: The best approach to GraphQL Schema Design 🍓</h3>
+              <h3 class="text-lg font-medium text-gray-700">Strawberry: The best approach to GraphQL Schema Design 🍓</h3>
               <span class="block text-gray-600 font-light text-sm">15 Oct 2019</span>
             </div>
           </a>
