c2726332b17b445492a73648dcb90b492ad8f75e
Nahuel Ambrosini <nambrosini@onapsis.com>
2021-02-13 14:02:05 -0300

    🔧 Update config to match v2 defaults

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-    // Hold until tailwind 2.0 darkMode: 'media',
+    darkMode: 'media',
     purge: [
       './src/**/*.html',
       './src/**/*.vue',
     ],
-    experimental: {
-      darkModeVariant: true
-    },
-    future: {
-      purgeLayersByDefault: true,
-    },
     theme: {},
     variants: {},
     plugins: [],
