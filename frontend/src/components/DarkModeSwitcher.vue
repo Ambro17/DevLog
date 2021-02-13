6bb8721b59eaf65b92088b85baa17e8283500e0d
Nahuel Ambrosini <nambrosini@onapsis.com>
2021-02-13 14:31:21 -0300

    :sty: Reduce switcher circle for more minimalist look

--- frontend/src/components/DarkModeSwitcher.vue
+++ frontend/src/components/DarkModeSwitcher.vue
@@ -1,7 +1,10 @@
 <template>
 <!-- This is an example component -->
 <label class="flex items-center">
-  <input class="relative w-10 h-5 transition-all duration-200 ease-in-out bg-gray-400 rounded-full shadow-inner outline-none appearance-none " type="checkbox" checked />
+  <input 
+      class="relative w-10 h-5 transition-all duration-200 ease-in-out bg-gray-400 rounded-full shadow-inner outline-none appearance-none " 
+      type="checkbox" 
+  />
 </label>
 </template>
 
@@ -14,7 +17,7 @@
     border-radius: 50%;
     top: 0;
     left: 0;
-    transform: scale(1.1);
+    transform: scale(0.7);
     box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.2);
     background-color: white;
     transition: .2s ease-in-out;
