4dc37cf4b66fc625763f304f49e1646a9f0ae0ce
Ambro17 <ambro17.1@gmail.com>
2021-02-15 22:48:50 -0300

    🖌 Make indigo level lower on dark mode

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -106,7 +106,7 @@
         <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-100">{{ $t('posts_header') }}</h2>
 
         <div class="flex flex-col items-center justify-center mt-6">
-          <a class="max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" 
+          <a class="max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 dark:border-indigo-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" 
              href="#">
             <div class="flex items-center justify-between px-4 py-2">
               <h3 class="text-lg text-center font-medium text-gray-700 dark:text-gray-300">Why i use emojis in all my git commits 🙌</h3>
@@ -114,14 +114,14 @@
             </div>
           </a>
 
-          <a class="mt-8 max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
+          <a class="mt-8 max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 dark:border-indigo-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
             <div class="flex items-center justify-between px-4 py-2">
               <h3 class="text-lg font-medium text-gray-700 dark:text-gray-300">Direnv: Automate your environment variables handling 🗜️</h3>
               <span class="block text-gray-600 dark:text-gray-100 font-light text-sm">29 Oct 2019</span>
             </div>
           </a>
 
-          <a class="mt-8 max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
+          <a class="mt-8 max-w-2xl w-full block bg-white dark:bg-gray-700 shadow-md rounded-md border-t-4 border-indigo-600 dark:border-indigo-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" href="#">
             <div class="flex items-center justify-between px-4 py-2">
               <h3 class="text-lg font-medium text-gray-700 dark:text-gray-300">Strawberry: The best approach to GraphQL Schema Design 🍓</h3>
               <span class="block text-gray-600 dark:text-gray-100 font-light text-sm">15 Oct 2019</span>
