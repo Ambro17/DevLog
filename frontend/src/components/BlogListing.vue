36dc516460e07ee7e76405e7b29ac46caebc8dff
Ambro17 <Ambro17.1@gmail.com>
2021-02-18 21:48:03 -0300

    🔧

--- frontend/src/components/BlogListing.vue
+++ frontend/src/components/BlogListing.vue
@@ -23,7 +23,7 @@
           <div class="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
             <div class="flex-1 bg-white dark:bg-gray-700  rounded-b-none overflow-hidden shadow-lg">
               <p class="w-full text-gray-600 dark:text-gray-300 text-xs md:text-sm pt-6 px-6">GETTING STARTED</p>
-              <div class="w-full font-bold text-xl text-gray-900 dark:200 px-6">👋 Welcome fellow Tailwind CSS and Ghost fan</div>
+              <div class="w-full font-bold text-xl text-gray-900 dark:text-gray-200 px-6">👋 Welcome fellow Tailwind CSS and Ghost fan</div>
               <p class="text-gray-800 dark:text-gray-300 font-serif text-base px-6 mb-5">
                 This starter template is an attempt to replicate the default Ghost theme "Casper" using Tailwind CSS and vanilla Javascript.
               </p>
