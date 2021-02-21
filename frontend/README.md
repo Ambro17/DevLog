2d3aadafb4e25f3e97a75bd235b14d0da1d37017
Ambro17 <Ambro17.1@gmail.com>
2021-02-21 11:54:13 -0300

    🏗️ Work in Progress

--- frontend/README.md
+++ frontend/README.md
@@ -6,8 +6,8 @@
 
 ## Credits
 
-Tutorial adapted from this [great post](https://dev.to/vonagedev/using-tailwind-css-with-vue-js-b1b)
-Using Ghostwind template from [here](https://github.com/tailwindtoolbox/Ghostwind)
+Tutorial started from this [great post](https://dev.to/vonagedev/using-tailwind-css-with-vue-js-b1b)
+Using templates from [here](https://tailwindcomponents.com/component/developer-portfolio) and [here](https://github.com/tailwindtoolbox/Ghostwind)
 
 ## TODO
  [x] Create Header and Footer vue components
@@ -17,10 +17,10 @@ Using Ghostwind template from [here](https://github.com/tailwindtoolbox/Ghostwin
 
 ## Improvements
  [x] Fine-tune site performance with lighthouse - Score of 95+
- [ ] Make lead layout finish before about page with some css property
- [ ] Auto detect user language and adjust accordingly
+ [ ] Add call to action below profile pic (and social network badges?)
+ [x] Auto detect user language and adjust accordingly
  [ ] Use flag svgs instead of emojis to have consistent ui over different fonts on language switcher
- [ ] Make header sticky
+ [x] Make header sticky
 
 ## Hacks
  [ ] Replace margin with div and border bottom to create extended space with background color
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -15,7 +15,7 @@
       dark:border-gray-900
       "
     >
-      <div class="max-w-2xl px-6 text-center mx-auto">
+      <div class="max-w-2xl space-y-8 px-6 text-center mx-auto mt-10">
         <h2 
           class="text-3xl font-semibold text-gray-800 dark:text-gray-50">
           {{ $t('home.intro.pre.hi') }}, 
@@ -28,7 +28,8 @@
             {{ $t('home.intro.pre.nice') }}
         </h2>
 
-        <i18n path="home.intro.full_text" tag="p" class="text-gray-600 dark:text-gray-50 mt-4">
+        <i18n path="home.intro.full_text" tag="p" 
+          class="text-xl leading-loose text-gray-600 dark:text-gray-50 mt-4">
           <template #br>
             <br>
           </template>
@@ -76,6 +77,14 @@
               >
             </div>
           </div>
+        </div>
+        <div class="button-container">
+          <a href="https://www.linkedin.com/in/nahuel-ambrosini-65a625b2/" class="bg-blue-500 rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600" target="_blank">
+              {{ $t('lets_talk') }}
+              <svg class="inline ml-2 w-6 stroke-current text-white stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+              </svg>
+          </a>
         </div>
       </div>
       <!-- Extra Div to mimic a background-colorable mb-10-->
