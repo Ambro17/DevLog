64579dcaa1df7199a5c3252a3710bccd373f334e
Ambro17 <Ambro17.1@gmail.com>
2021-03-20 17:58:16 -0300

    ✨Add save posts script

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,34 @@
 # Collect all static posts
+import json
+from pathlib import Path
+
 from devlog.schema import schema
 
-schema.execute_sync(
-    """
-    {
-        posts {
-            
+DATA = Path(__file__).parent / 'data'
+
+
+def save_posts():
+    result = schema.execute_sync(
+        """
+        {
+            posts {
+            topic
+            title
+            preview
+            readingTime
+            content
+            author {
+                name
+            } 
+            }
         }
-    }
-    """
-)
+        """
+    )
+    if result.errors:
+        raise ValueError(result.errors[0].message)
+
+    with open(DATA/'posts.json', 'w', encoding='utf-8') as f:
+        json.dump(result.data['posts'], f, indent=4, ensure_ascii=False)
+
+
+save_posts()
--- backend/src/devlog/schema.py
+++ backend/src/devlog/schema.py
@@ -11,47 +11,17 @@ from devlog.domain import Author, Me, Post, Project, Talk
 class Query:
 
     @strawberry.field
-    def me(self) -> Me:
-        return Me(
-            name='Nahuel',
-            intro=textwrap.dedent("""\
-                I'm a software engineer who ❤ Python & GraphQL.
-                📚 My programming interests are now mainly focused on
-                API Design, Serverless Architectures, and DevOps
-                💼 I currently work at Onapsis as a Sr Python Developer
-                And on my free time i like to learn Frontend development with Vue 🧪
-            """),
-            about=textwrap.dedent("""\
-                I'm a software engineer who loves to work on complex problem domains, 
-                follower of Domain Driven Design (DDD) practices because i believe 
-                it helps to generate a synergic workflow between project owners and developers.
-                Having deep domain knowledge and a good communication system helps delivering high quality software,
-                that's flexible to changes, and has predictable delivery times.
-            """)
-        )
-
-    @strawberry.field
     def posts(self) -> List[Post]:
-        return [Post(title="Ambro", content='Lorem Ipsum', author=Author('Me'))]
-
-    @strawberry.field
-    def post_by_id(self, id: str) -> Post:
-        return Post(title="Ambro", content='Lorem Ipsum', author=Author('Me'))
-
-    @strawberry.field
-    def talks(self) -> List[Talk]:
         return [
-            Talk(
-                title='Pythonic APIs with GraphQL',
-                description='Build easy to use APIs with GraphQL',
-                tags=['Python', 'GraphQL', 'API', 'Design'],
-                video_url='https://www.youtube.com/watch?v=ePSRREuTZq4'
-            )
-        ]
+            Post(
+                topic='Python',
+                title="Ambro",
+                preview='...',
+                content='Lorem Ipsum',
+                reading_time=5,
+                author=Author('Me')),
+            ]
 
-    @strawberry.field
-    def projects(self, id: str) -> List[Project]:
-        return [Project(name='Slackify', description='Build modern slack bots', url='https://github.com/Ambro17/slackify')]
 
 
 schema = strawberry.Schema(query=Query)
