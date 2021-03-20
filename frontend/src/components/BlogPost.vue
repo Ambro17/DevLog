e816e0f7e068f92f6130548339f7b051c7cfb8e8
Ambro17 <Ambro17.1@gmail.com>
2021-03-20 19:20:11 -0300

    ✨Parametrize blog post

--- frontend/src/components/BlogPost.vue
+++ frontend/src/components/BlogPost.vue
@@ -6,8 +6,14 @@
     
     <!--Title-->
     <div class="text-center pt-16 md:pt-32">
-      <p class="text-sm md:text-base text-green-500 font-bold">08 APRIL 2019 <span class="text-gray-900">/</span> GETTING STARTED</p>
-      <h1 class="font-bold break-normal text-3xl dark:text-gray-300 md:text-5xl">Welcome to Ghostwind CSS</h1>
+      <p class="text-sm md:text-base text-green-500 font-bold"> 
+        {{ new Intl.DateTimeFormat('en-US', {day: "numeric", month: "long", year:"numeric"}).format(
+            new Date(post.publishedOn)
+          ) 
+        }} 
+        <span class="text-gray-900">/</span> {{post.topic}} 
+      </p>
+      <h1 class="font-bold break-normal text-3xl dark:text-gray-300 md:text-5xl">{{ post.title }}</h1>
     </div>
 
     <!--image-->
@@ -28,32 +34,9 @@
             👋 Welcome fellow <a href="https://www.tailwindcss.com">Tailwind CSS</a> and <a href="https://www.ghost.org">Ghost</a> fan.  This starter template is an attempt to replicate the default Ghost theme <a href="https://demo.ghost.io/welcome">"Casper"</a> using Tailwind CSS and vanilla Javascript.
           </p>
 
-          <p class="py-6">The basic blog page layout is available and all using the default Tailwind CSS classes (although there are a few hardcoded style tags). If you are going to use this in your project, you will want to convert the classes into components.</p>				
+          <p class="py-6">{{ post.content }}</p>				
         
-          <p class="py-6">Sed dignissim lectus ut tincidunt vulputate. Fusce tincidunt lacus purus, in mattis tortor sollicitudin pretium. Phasellus at diam posuere, scelerisque nisl sit amet, tincidunt urna. Cras nisi diam, pulvinar ut molestie eget, eleifend ac magna. Sed at lorem condimentum, dignissim lorem eu, blandit massa. Phasellus eleifend turpis vel erat bibendum scelerisque. Maecenas id risus dictum, rhoncus odio vitae, maximus purus. Etiam efficitur dolor in dolor molestie ornare. Aenean pulvinar diam nec neque tincidunt, vitae molestie quam fermentum. Donec ac pretium diam. Suspendisse sed odio risus. Nunc nec luctus nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec nulla eget sem dictum elementum.</p>
-
-          <ol>
-            <li class="py-3">Maecenas accumsan lacus sit amet elementum porta. Aliquam eu libero lectus. Fusce vehicula dictum mi. In non dolor at sem ullamcorper venenatis ut sed dui. Ut ut est quam. Suspendisse quam quam, commodo sit amet placerat in, interdum a ipsum. Morbi sit amet tellus scelerisque tortor semper posuere.</li>
-            <li class="py-3">Morbi varius posuere blandit. Praesent gravida bibendum neque eget commodo. Duis auctor ornare mauris, eu accumsan odio viverra in. Proin sagittis maximus pharetra. Nullam lorem mauris, faucibus ut odio tempus, ultrices aliquet ex. Nam id quam eget ipsum luctus hendrerit. Ut eros magna, eleifend ac ornare vulputate, pretium nec felis.</li>
-            <li class="py-3">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc vitae pretium elit. Cras leo mauris, tristique in risus ac, tristique rutrum velit. Mauris accumsan tempor felis vitae gravida. Cras egestas convallis malesuada. Etiam ac ante id tortor vulputate pretium. Maecenas vel sapien suscipit, elementum odio et, consequat tellus.</li>
-          </ol>
-
-          <blockquote class="border-l-4 border-green-500 italic my-8 pl-8 md:pl-12">Example of blockquote - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.</blockquote>
-
-          <p class="py-6">Example code block:</p>
-          <pre class="bg-gray-900 rounded text-white font-mono text-base p-4">
-            <code class="break-words whitespace-pre-wrap">
-  &lt;header class="site-header outer"&gt;
-    &lt;div class="inner"&gt;
-      {{"&lt;p>Hello World!&lt;/p>"}}
-    &lt;/div&gt;
-  &lt;/header&gt;
-            </code>
-          </pre>
-
-                          
-          <!--/ Post Content-->
-              
+                        
         </div>
           <!--Author-->
           <div class="flex w-full items-center font-sans p-8 md:p-12">
@@ -82,6 +65,21 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data: function() {
+    return {
+      post: {
+          "topic": "🐍 Python",
+          "title": "This is some post title",
+          "preview": "This starter template is an attempt to replicate the default Ghost theme \"Casper\" using Tailwind CSS and vanilla Javascript.",
+          "readingTime": 5,
+          "content": "This is the blog content ...",
+          "author": {
+            "name": "Me"
+          },
+          "publishedOn": "2020-01-17"
+      },
+    }
   }
 }
 </script>
